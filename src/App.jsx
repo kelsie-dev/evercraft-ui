ac033105c950d37ecaca745f1d8a9d4b7b91e038
kelsie-dev <kelsie.cummings@accenture.com>
2024-09-19 10:55:09 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useEffect } from 'react'
+import './App.css'
+
+function App() {
+
+
+  useEffect(() => {
+
+  }, [])
+
+  return (
+    <>
+      <h1>Welcome to Evercraft!</h1>
+    </>
+  )
+}
+
+export default App
